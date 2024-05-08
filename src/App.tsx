17ac3e4a1022f63d6eae47bb4b169e5ff6177d2b
정세훈 <wer5136@naver.com>
2024-05-08 22:30:14 +0900

    모집글 페이지 작성자 입장 1차

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import RecruitmentPage from './pages/RecruitmentPage';
+import RecruitmentPage_Oner from './pages/RecruitmentPage_Oner';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="Main" element={<MainPage />} />
           <Route path="Recruitment" element={<RecruitmentPage />} />
+          <Route path="Recruitment_Oner" element={<RecruitmentPage_Oner />} />
         </Routes>
       </BrowserRouter>
     </>
