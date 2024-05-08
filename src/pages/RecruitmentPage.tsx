484bc9b857ed0729cf7043697197dfeeb0f5325d
정세훈 <wer5136@naver.com>
2024-05-08 21:34:14 +0900

    모집글 페이지 1차

--- src/pages/RecruitmentPage.tsx
+++ src/pages/RecruitmentPage.tsx
@@ -9,38 +9,54 @@ function RecruitmentPage() {
       </button>
       <div className="flex flex-col ml-20">
         <div className="text-2xl font-bold mb-6">[디자이너] 신규 사이드 프로젝트 팀원을 모집합니다.</div>
-        <div className="flex flex-row items-center gap-4">
+        <div className="flex flex-row items-center gap-4 mb-10">
           <img src={Profile}></img>
           <div className="text-lg font-bold">정세훈</div>
           <div className="text-xl">|</div>
           <div className="text-lg font-thin text-[#6A6A6A]">2024.04.19</div>
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-4">
-        <div className="text-gray-700">
-          <p>
-            <strong>프로젝트 명칭:</strong> 디자이니 신규 사이트 프로젝트 팀원을 모집합니다.
-          </p>
+      <div className="border-t border-4 border-[#D9D9D9] flex-grow mb-10"></div>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="flex flex-row gap-10">
+          <div className="text-gray-700 text-">
+            <strong>모집 구분</strong>
+          </div>
+          <div className="text-gray-700 text-">
+            <strong>프로젝트</strong>
+          </div>
         </div>
-        <div className="text-gray-700">
-          <p>
-            <strong>모집 인원:</strong> 2명
-          </p>
+        <div className="flex flex-row gap-10">
+          <div className="text-gray-700 text-">
+            <strong>모집 인원</strong>
+          </div>
+          <div className="text-gray-700 text-">
+            <strong>2명</strong>
+          </div>
         </div>
-        <div className="text-gray-700">
-          <p>
-            <strong>프로젝트 날짜:</strong> 2024.04.19
-          </p>
+        <div className="flex flex-row gap-10">
+          <div className="text-gray-700 text-">
+            <strong>모집 분야</strong>
+          </div>
+          <div className="text-gray-700 text-">
+            <strong>백엔드</strong>
+          </div>
         </div>
-        <div className="text-gray-700">
-          <p>
-            <strong>모집 분야:</strong> 백엔드, 에세이 기간
-          </p>
+        <div className="flex flex-row gap-10">
+          <div className="text-gray-700 text-">
+            <strong>모집 구분</strong>
+          </div>
+          <div className="text-gray-700 text-">
+            <strong>프로젝트</strong>
+          </div>
         </div>
-        <div className="text-gray-700">
-          <p>
-            <strong>예상 기간:</strong> 3개월
-          </p>
+        <div className="flex flex-row gap-10">
+          <div className="text-gray-700 text-">
+            <strong>모집 구분</strong>
+          </div>
+          <div className="text-gray-700 text-">
+            <strong>프로젝트</strong>
+          </div>
         </div>
       </div>
       <div className="flex justify-end mt-8">
