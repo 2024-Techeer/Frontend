de8ad2a03e803633a6dd7700f86875ecb18a0a7f
LeeYunseo04 <129928913+LeeYunseo04@users.noreply.github.com>
2024-05-17 19:34:52 +0900

    마이페이지(정보수정)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
-
+import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import MyUserEditPage from './pages/MyUserEditPage';
 import RecruitmentPage from './pages/RecruitmentPage';
 import RecruitmentPage_Owner from './pages/RecruitmentPage_Owner';
 import WritePage from './pages/WritePage';
@@ -12,9 +12,9 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="Login" element={<LoginPage />} />
           <Route path="Main" element={<MainPage />} />
-
+          <Route path="Login" element={<LoginPage />} />
+          <Route path="MyUserEdit" element={<MyUserEditPage />} />
           <Route path="Register" element={<RegisterPage />} />
           <Route path="Recruitment" element={<RecruitmentPage />} />
           <Route path="Recruitment_Owner" element={<RecruitmentPage_Owner />} />
