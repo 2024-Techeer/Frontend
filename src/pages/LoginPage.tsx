993d214f760ce6be5dbf032d3b2f46bd46af9884
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-06-20 23:50:21 +0900

    fix: request url 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -34,7 +34,7 @@ const LoginPage: React.FC = () => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const response = await fetch('http://backend/api/v1/auth/login', {
+    const response = await fetch('http://localhost/api/v1/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
